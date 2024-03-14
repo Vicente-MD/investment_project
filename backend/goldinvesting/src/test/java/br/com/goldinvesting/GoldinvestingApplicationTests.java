bab1e8ba39ba4a319d991b82a857348fb4e62434
vicente-md <vicentemeidam@gmail.com>
2024-03-13 21:44:41 -0300

    First commit

---
+++ backend/goldinvesting/src/test/java/br/com/goldinvesting/GoldinvestingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goldinvesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldinvestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
